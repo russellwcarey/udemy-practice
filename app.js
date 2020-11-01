21e70e8c1e68cd3f73e2cffd443764b022163971
Russell Carey <russellwcarey@gmail.com>
2020-11-01 15:28:09 -0700

    added js, json, html, css files, updated json files with dummy json data

---
+++ app.js
@@ -0,0 +1,26 @@
+//Datatype - many different types
+
+//string -> text -> "" or ''
+
+//boolean or bool - true or false
+//if you are evaluating something for true or false
+
+//integer or int -> 1234 ('1234' is no longer an int, it is now a string)
+
+//array -> collection of things
+//array -> [ ] -> [ 'hello', 123, true, { 'name': 'Jason'} ]
+
+//Object -> wrapped in { key: value } pair
+// {
+//     'name': 'Jason',
+//         'favoriteColor': 'blue'
+// }
+
+
+
+{
+    "firstName" : "Russ",
+        "lastName" : "Carey",
+            
+        
+}
